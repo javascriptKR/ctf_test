b5fc68ab54b878d1a5be6791bf051d38429f1974
Mollang2 <62112908+javascriptKR@users.noreply.github.com>
2022-03-17 12:04:48 +0900

    re

--- csrf/prob/app.py
+++ csrf/prob/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, render_template
-from selenium import webdriver
 import urllib
 import os
 
@@ -11,56 +10,13 @@ try:
 except:
     FLAG = "[**FLAG**]"
 
-
-def read_url(url, cookie={"name": "name", "value": "value"}):
-    cookie.update({"domain": "127.0.0.1"})
-    try:
-        options = webdriver.ChromeOptions()
-        for _ in [
-            "headless",
-            "window-size=1920x1080",
-            "disable-gpu",
-            "no-sandbox",
-            "disable-dev-shm-usage",
-        ]:
-            options.add_argument(_)
-        driver = webdriver.Chrome("/chromedriver", options=options)
-        driver.implicitly_wait(3)
-        driver.set_page_load_timeout(3)
-        driver.get("http://127.0.0.1:8000/")
-        driver.add_cookie(cookie)
-        driver.get(url)
-    except Exception as e:
-        driver.quit()
-        print(str(e))
-        # return str(e)
-        return False
-    driver.quit()
-    return True
-
 @app.route('/')
 def index():
     return render_template("index.html")
 
-def check_csrf(param, cookie={"name": "name", "value": "value"}):
-    url = f"http://127.0.0.1:8000/vuln?param={urllib.parse.quote(param)}"
-    return read_url(url, cookie)
-
-@app.route('/vuln')
-def vuln():
-    return 
-
-
-@app.route('/email', method=["GET", "POST"])
+@app.route('/email')
 def email():
-    if request.method == "GET":
-        return render_template("email.html")
-    elif request.method == "POST":
-        param = request.form.get("param", "")
-        if not check_csrf(param):
-            return '<script>alert("wrong??");history.go(-1);</script>'
-
-        return '<script>alert("good");history.go(-1);</script>'
+    return render_template("email.html")
 
 memo_text = ""
 
@@ -71,7 +27,6 @@ def memo():
     if text:
         memo_text += text
     return render_template("memo.html", memo=memo_text)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
